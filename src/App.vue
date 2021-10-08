f9dc1eeba8f041304c9ad6077cef73f418b36d3b
Der Alex <alexander.thimm@googlemail.com>
2021-10-08 23:37:20 +0200

    added some initial styling with tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup></script>
 
 <template>
-  <header class="shadow-md">
-    <p>this will be the header</p>
-  </header>
-  <aside>this will be the sidebar</aside>
-  <main>
-    <router-view />
-  </main>
-  <footer>maybe some footer</footer>
+  <div class="bg-gray-700 text-gray-200 h-screen w-screen">
+    <header class="bg-gray-800">
+      <p>this will be the header</p>
+    </header>
+    <aside class="h-full w-3/12 bg-gray-900">this will be the sidebar</aside>
+    <main>
+      <router-view />
+    </main>
+    <footer>maybe some footer</footer>
+  </div>
 </template>
 
 <style lang="scss">
