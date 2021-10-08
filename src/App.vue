07cc3b5f6f6b204bebec75c1157c782dc561f1b9
Der Alex <alexander.thimm@googlemail.com>
2021-10-08 22:16:42 +0200

    Initial commit - installed vue@next with vite - added vue-router@next - added vuex@next - added tailwind - created initial route to home - created initial store

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <header class="shadow-md">
+    <p>this will be the header</p>
+  </header>
+  <aside>this will be the sidebar</aside>
+  <main>
+    <router-view />
+  </main>
+  <footer>maybe some footer</footer>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
